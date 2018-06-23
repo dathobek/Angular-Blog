3bbe7b1291470034e770c82a54a824071458e5cd
David Athobe <dathobek@gmail.com>
2018-06-23 12:31:21 +0100

    material-module/updated

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import {MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule} from '@angular/material';
 
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
+  exports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
   declarations: []
 })
 export class MaterialModule { }
