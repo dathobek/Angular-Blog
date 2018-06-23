823a68948f03c5278f065fad9ba78dc7f28cbd76
David Athobe <dathobek@gmail.com>
2018-06-23 19:29:15 +0100

    material-module-added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule} from '@angular/material';
+import {MatTableModule,MatButtonModule,MatCardModule} from '@angular/material';
 
 
 @NgModule({
-  imports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
-  exports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule],
+  imports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,
+  MatTableModule,MatButtonModule,MatCardModule],
+  exports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,
+  MatTableModule,MatButtonModule,MatCardModule],
   declarations: []
 })
 export class MaterialModule { }
