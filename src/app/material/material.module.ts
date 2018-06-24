72b3c0b51d91cdce9652b1aec158a3ab97e68510
David Athobe <dathobek@gmail.com>
2018-06-24 21:52:18 +0100

    ExtraDialog

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import {MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule} from '@angular/material';
 import {MatTableModule,MatButtonModule,MatCardModule} from '@angular/material';
+import {MatDialogModule,MatSelectModule,MatInputModule} from '@angular/material'
 
 
 @NgModule({
   imports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,
-  MatTableModule,MatButtonModule,MatCardModule],
+  MatTableModule,MatButtonModule,MatCardModule,MatDialogModule,MatSelectModule,MatInputModule],
   exports: [MatSidenavModule,MatToolbarModule,MatIconModule,MatListModule,
-  MatTableModule,MatButtonModule,MatCardModule],
+  MatTableModule,MatButtonModule,MatCardModule,MatDialogModule,MatSelectModule,MatInputModule],
   declarations: []
 })
 export class MaterialModule { }
